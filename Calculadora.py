857ffe01f6bc6d1063db4418474b0b5ba5e1e17b
Ryuz <jrafel939@gmail.com>
2026-06-03 01:40:06 -0300

    Atualiza sistema

--- Calculadora.py
+++ Calculadora.py
@@ -1,28 +1,32 @@
-print("===========Calculadora=============")
+def main():
+    print("===========Calculadora=============")
 
-Calcular = str(input("Qual tipo de Calculo? Soma/Divisão/Subtração/Multiplicação: "))
-#Escolher o tipo
-if Calcular == "Soma":
-    Numero1 = int(input("[1]: "))
-    Numero2 = int(input("[2]: "))
-    Soma = Numero1 + Numero2
-    print("Resultado:", Soma)
-#Divisão
-elif Calcular == "Divisão":
-    Numero1 = int(input("[1]: "))
-    Numero2 = int(input("[2]: "))
-    Divisão = Numero1 / Numero2
-    print("Resultado:", Divisão)
-#Subtração
-elif Calcular == "Subtração":
-    Numero1 = int(input("[1]: "))
-    Numero2 = int(input("[2]: "))
-    Subtração = Numero1 - Numero2
-    print("Resultado", Subtração)
-#Multiplicação
-elif Calcular == "Multiplicação":
-    Numero1 = int(input("[1]: "))
-    Numero2 = int(input("[2]: "))
-    Multiplicação = Numero1 * Numero2
-    print("Resultado:", Multiplicação)
+    while True:
+        Calcular = str(input("Qual tipo de Calculo? Soma/Divisão/Subtração/Multiplicação: "))
+        #Escolher o tipo
+        if Calcular == "Soma":
+            Numero1 = int(input("[1]: "))
+            Numero2 = int(input("[2]: "))
+            Soma = Numero1 + Numero2
+            print("Resultado:", Soma)
+        #Divisão
+        elif Calcular == "Divisão":
+            Numero1 = int(input("[1]: "))
+            Numero2 = int(input("[2]: "))
+            Divisão = Numero1 / Numero2
+            print("Resultado:", Divisão)
+        #Subtração
+        elif Calcular == "Subtração":
+            Numero1 = int(input("[1]: "))
+            Numero2 = int(input("[2]: "))
+            Subtração = Numero1 - Numero2
+            print("Resultado", Subtração)
+        #Multiplicação
+        elif Calcular == "Multiplicação":
+            Numero1 = int(input("[1]: "))
+            Numero2 = int(input("[2]: "))
+            Multiplicação = Numero1 * Numero2
+            print("Resultado:", Multiplicação)
+if __name__ == "__main__":
+    main()
 
